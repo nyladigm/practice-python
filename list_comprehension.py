55fc3446f75672aae6f59218d077b85ba1f4724a
Laura Schuler <nyladigm@users.noreply.github.com>
2019-06-04 14:27:04 -0700

    Exercise 7 - List Comprehension

---
+++ list_comprehension.py
@@ -0,0 +1,10 @@
+'''
+Exercise 7 - List Comprehensions from Practice Python
+Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100].
+Write one line of Python that takes this list a and makes a new list that has only the even elements of this list in it.
+'''
+
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+x = [item for item in a if item % 2 == 0]
+print(x)
